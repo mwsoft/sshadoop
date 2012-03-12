a6db7e3556d59614a2624988cd80e2f12a4264b4
mwsoft <msorwk@yahoo.co.jp>
2012-03-13 02:30:21 +0900

    now coding

--- example/src/main/scala/jp/mwsoft/sshadoop/mapreduce/example/SimpleWordCount.scala
+++ example/src/main/scala/jp/mwsoft/sshadoop/mapreduce/example/SimpleWordCount.scala
@@ -1,42 +1,28 @@
 package jp.mwsoft.sshadoop.mapreduce.example
 
-import org.apache.hadoop.util.Tool
-import org.apache.hadoop.conf.{ Configured, Configuration }
-import org.apache.hadoop.mapreduce.Job
-import org.apache.hadoop.io.Text
-import org.apache.hadoop.io.LongWritable
-import org.apache.hadoop.mapreduce.Mapper
-import org.atilika.kuromoji.Tokenizer
+import org.apache.hadoop.io._
 import jp.mwsoft.sshadoop.mapreduce._
+import jp.mwsoft.sshadoop.util.STool
 
-object WordCountSample extends Configured with Tool with App {
+object WordCountSample extends STool {
 
-  run(args)
-
-  override def run(args: Array[String]): Int = {
-
-    val job = new SJob(getConf(), "jobName")
-
-    job mapper (new MyMapper()) reducer (new MyReducer())
-
-    if (job.waitForCompletion(true)) 0 else 1
+  override def runJob( args: Array[String] ): Boolean = {
+    new SJob( getConf(), "jobName" )
+      .mapper( new MyMapper() )
+      .reducer( new MyReducer() )
+      .combiner( new MyReducer() )
+      .fileInputPaths( "data/in" )
+      .fileOutputPath( "data/out" )
+      .waitForCompletion( true )
   }
 
   class MyMapper extends SMapper[LongWritable, Text, Text, LongWritable] {
-    override def map(key: LongWritable, value: Text, context: Context) {
-      for (str <- value.split("\t")) context.write(str, 1)
-    }
+    override def map( key: LongWritable, value: Text, context: Context ) =
+      for ( str <- value.split( "\\s" ) ) context.write( str, 1 )
   }
 
   class MyReducer extends SReducer[Text, LongWritable, Text, LongWritable] {
-    override def reduce(key: Text, values: Iterator[LongWritable], context: Context) {
-      context.write(key, values.map(_.get).sum)
-    }
-  }
-
-  class MyCombiner extends SReducer[Text, LongWritable, Text, LongWritable] {
-    override def reduce(key: Text, values: Iterator[LongWritable], context: Context) {
-      context.write(key, values.map(_.get).sum)
-    }
+    override def reduce( key: Text, values: Iterator[LongWritable], context: Context ) =
+      context.write( key, values.map( _.get ).sum )
   }
 }
