7cfbf3566d79b069501e8e9f218079179b2df537
masato <msorwk@yahoo.co.jp>
2012-03-13 21:37:14 +0900

    now coding

--- example/src/main/scala/jp/mwsoft/sshadoop/mapreduce/example/JaWordCountSample.scala
+++ example/src/main/scala/jp/mwsoft/sshadoop/mapreduce/example/JaWordCountSample.scala
@@ -5,6 +5,8 @@ import org.apache.hadoop.io.{ Text, LongWritable }
 import org.atilika.kuromoji.Tokenizer
 import jp.mwsoft.sshadoop.mapreduce._
 import jp.mwsoft.sshadoop.util.STool
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat
+import org.apache.hadoop.io.BytesWritable
 
 /**
  * 日本語用ワードカウント。Kuromoji利用
@@ -12,25 +14,35 @@ import jp.mwsoft.sshadoop.util.STool
  */
 object JaWordCountSample extends STool {
 
-  def runJob( args: Array[String] ): Boolean = {
-    new SJob( getConf(), "jobName" )
-      .mapper( new JaWordCountMapper() )
-      .reducer( new JaWordCountReducer() )
-      .fileInputPaths( "data/in" )
-      .fileOutputPath( "data/out/" + currentTime )
-      .waitForCompletion( true )
+  def runJob(args: Array[String]): Boolean = {
+    new SJob(getConf(), "jobName")
+      .mapper(new JaWordCountMapper())
+      .reducer(new JaWordCountReducer())
+      .combiner(new JaWordCountCombiner())
+      .inputFormatClass(classOf[SequenceFileInputFormat[BytesWritable, Text]])
+      .fileInputPaths("/user/hive/warehouse/uni_tweets/dt=20120201")
+      .fileOutputPath("data/out/" + currentTime)
+      .waitForCompletion(true)
   }
 
-  class JaWordCountMapper extends SMapper[LongWritable, Text, Text, LongWritable] {
+  class JaWordCountMapper extends SMapper[BytesWritable, Text, Text, LongWritable] {
     val tokenizer = Tokenizer.builder().userDictionary(getClass.getResourceAsStream("user.dic")).build()
-    override def map( key: LongWritable, value: Text, context: Context ) {
-      tokenizer.tokenize( value ).iterator() foreach ( x => context.write( x.getSurfaceForm(), 1L ) )
+    override def map(key: BytesWritable, value: Text, context: Context) {
+      tokenizer.tokenize(value).iterator() foreach (x => context.write(x.getSurfaceForm(), 1L))
     }
   }
 
   class JaWordCountReducer extends SReducer[Text, LongWritable, Text, LongWritable] {
-    override def reduce( key: Text, values: Iterator[LongWritable], context: Context ) {
-      context.write( key, ( 0L /: values ) { _ + _ } )
+    override def reduce(key: Text, values: Iterator[LongWritable], context: Context) {
+      val sum = (0L /: values) { _ + _ }
+      if (sum >= 10) context.write(key, sum)
+    }
+  }
+
+  class JaWordCountCombiner extends SReducer[Text, LongWritable, Text, LongWritable] {
+    override def reduce(key: Text, values: Iterator[LongWritable], context: Context) {
+      val sum = (0L /: values) { _ + _ }
+      if (sum >= 2) context.write(key, sum)
     }
   }
 }
