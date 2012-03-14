0db6c188cae85bbaa73f5e682ab1384ca57edfed
masato <msorwk@yahoo.co.jp>
2012-03-14 21:14:34 +0900

    now coding...

---
+++ example/src/main/scala/jp/mwsoft/sshadoop/mapreduce/example/Pair.scala
@@ -0,0 +1,57 @@
+package jp.mwsoft.sshadoop.mapreduce.example
+
+import jp.mwsoft.sshadoop.mapreduce._
+import org.apache.hadoop.io._
+import jp.mwsoft.sshadoop.util.STool
+
+object Pair extends STool {
+
+  def runJob(args: Array[String]): Boolean = {
+    new SJob(getConf(), "jobName")
+      .mapper(new PairMapper())
+      .reducer(new PairReducer())
+      .combiner(new PairCombiner())
+      .fileInputPaths("data/in")
+      .fileOutputPath("data/out/6")
+      .waitForCompletion(true)
+  }
+
+  class PairMapper extends SMapper[LongWritable, Text, Text, MapWritable] {
+    override def map(key: LongWritable, value: Text, context: Context) {
+      for (word <- value.split("\\s")) {
+        val map = new MapWritable()
+        map.put(word, 1L)
+        context.write(word, map)
+      }
+    }
+  }
+
+  def reduceMap(values: Iterator[MapWritable]): java.util.Map[Text, LongWritable] = {
+    val map = new java.util.HashMap[Text, LongWritable]()
+    for (value <- values; entry <- value.entrySet.iterator) {
+      val key = entry.getKey.asInstanceOf[Text]
+      val value = entry.getValue.asInstanceOf[LongWritable].get
+      println(Option(map.get(key)).getOrElse(0L))
+      map.put(key, Option(map.get(key)).getOrElse(0L))
+    }
+    map
+  }
+
+  class PairCombiner extends SReducer[Text, MapWritable, Text, MapWritable] {
+    override def reduce(key: Text, values: Iterator[MapWritable], context: Context) {
+      val map = reduceMap(values)
+      val mapWritable = new MapWritable()
+      mapWritable.putAll(map)
+      context.write(key, mapWritable)
+    }
+  }
+
+  class PairReducer extends SReducer[Text, MapWritable, Text, LongWritable] {
+    override def reduce(key: Text, values: Iterator[MapWritable], context: Context) {
+      val map = reduceMap(values)
+      for (entry <- map.entrySet.iterator)
+        context.write(key.toString + "," + entry.getKey, entry.getValue)
+    }
+  }
+
+}
